529542741adc2816c5661df3dde99339301f3f4e
nwpelletier <nickw.pelletier@gmail.com>
2025-03-21 17:54:11 -0400

    global css + semantic changes
    -using project panels to test this (because I've never done this before) using var(--project-panel-background) for example, setting this all in App.css, then we don't have to pick colors in child css files.
    
    Semantic naming: looked into this a bit, and instead of "divs" everywhere, we should be using "header" for titles, "figure" for images, etc.  Again, in App.css we can set their style back to default, and in our files, gradually change <div> everywhere to these.  Might require some fixes throughout but long term it's going to be easier to work with.
    
    -pulled down box shadow/border effect for project panels, based on Bobby's "looks like word perfect" comment lol.  I think boxing the elements off too much is not the look they're after.  so more floating elements in general.

--- ReactProject/cerasp/src/components/layouts/Header.js
+++ ReactProject/cerasp/src/components/layouts/Header.js
@@ -3,7 +3,7 @@ import "./css/Header.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="layout-header">
       <img src="/logos/cerasplogo.png" alt="CERASP Logo" />
       <Navigation />
     </header>
--- ReactProject/cerasp/src/components/panels/projectpanels/ProjectPanelB.js
+++ ReactProject/cerasp/src/components/panels/projectpanels/ProjectPanelB.js
@@ -10,14 +10,25 @@ const ProjectPanelB = ({
   headline,
 }) => {
   return (
-    <div className="project-panel-b-grid-layout">
-      <div className="top-image">{topImage}</div>
-      <div className="top-right-title">{title}</div>
-      <div className="middle-left-quote">{quote}</div>
-      <div className="bottom-left-image">{bottomLeftImage}</div>
-      <div className="bottom-center-image">{bottomCenterImage}</div>
-      <div className="bottom-right-headline">{headline}</div>
-    </div>
+    <>
+      {/* <div className="project-panel-b-grid-layout">
+        <div className="top-image">{topImage}</div>
+        <div className="top-right-title">{title}</div>
+        <div className="middle-left-quote">{quote}</div>
+        <div className="bottom-left-image">{bottomLeftImage}</div>
+        <div className="bottom-center-image">{bottomCenterImage}</div>
+        <div className="bottom-right-headline">{headline}</div>
+      </div> */}
+
+      <section className="project-panel-b-grid-layout">
+        <figure className="top-image">{topImage}</figure>
+        <header className="top-right-title">{title}</header>
+        <blockquote className="middle-left-quote">{quote}</blockquote>
+        <figure className="bottom-left-image">{bottomLeftImage}</figure>
+        <figure className="bottom-center-image">{bottomCenterImage}</figure>
+        <article className="bottom-right-headline">{headline}</article>
+      </section>
+    </>
   );
 };
 
