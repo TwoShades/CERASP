df16b558e043c573e1b1bb3fa6632c0a22e02777
nwpelletier <nickw.pelletier@gmail.com>
2025-03-24 17:50:20 -0400

    updates
    about overview, nav

--- ReactProject/cerasp/src/components/panels/AboutOverview.js
+++ ReactProject/cerasp/src/components/panels/AboutOverview.js
@@ -31,10 +31,10 @@ const AboutOverview = () => {
           technology, production, and Regulatory Affairs. As a trusted life
           sciences partner, we provide turnkey solutions to streamline product
           development and reduce commercialization risks. We also offer
-          industry-focused training and internships. A member of the CCTT
+          {/* industry-focused training and internships. A member of the CCTT
           network, CERASP is affiliated with John Abbott College and Cégep
           Gérald-Godin, delivering high-quality services through innovation and
-          expertise.
+          expertise. */}
         </p>
       </div>
 
@@ -43,15 +43,17 @@ const AboutOverview = () => {
 
       {/* Mission Section */}
       <div className="about-overview-mission">
-        <h2>MISSION</h2>
+        <h2 className="about-heading">MISSION</h2>
         <ul className={`mission-bullet-points ${showMission ? "hide" : ""}`}>
-          <li>Advance research</li>
-          <li>Develop solutions</li>
-          <li>Support advancements</li>
+          <li className="about-list-item">Advance research</li>
+          <li className="about-list-item">Develop solutions</li>
+          <li className="about-list-item">Support advancements</li>
         </ul>
-        <button onClick={toggleMission}>{showMission ? "-" : "+"}</button>
+        <button className="about-toggle-button" onClick={toggleMission}>
+          {showMission ? "-" : "+"}
+        </button>
         <div className={`mission-details ${showMission ? "open" : ""}`}>
-          <p>
+          <p className="about-overview-details-text">
             {`Advance research and training in pharmaceutical innovation to
             drive cutting-edge solutions, Develop and evaluate new solutions in
             biopharmaceutics and technology to enhance product quality, Support
@@ -63,15 +65,17 @@ const AboutOverview = () => {
 
       {/* Vision Section */}
       <div className="about-overview-vision">
-        <h2>VISION</h2>
+        <h2 className="about-heading">VISION</h2>
         <ul className={`vision-bullet-points ${showVision ? "hide" : ""}`}>
-          <li>Be a leading partner</li>
-          <li>Deliver innovations</li>
-          <li>Foster strong ties</li>
+          <li className="about-list-item">Be a leading partner</li>
+          <li className="about-list-item">Deliver innovations</li>
+          <li className="about-list-item">Foster strong ties</li>
         </ul>
-        <button onClick={toggleVision}>{showVision ? "-" : "+"}</button>
+        <button className="about-toggle-button" onClick={toggleVision}>
+          {showVision ? "-" : "+"}
+        </button>
         <div className={`vision-details ${showVision ? "open" : ""}`}>
-          <p>
+          <p className="about-overview-details-text">
             {`Be a leading partner in applied pharmaceutical research in Quebec
             and Canada by providing transformative solutions, Deliver innovative
             solutions to clients while ensuring their success, Foster strong
@@ -83,15 +87,17 @@ const AboutOverview = () => {
 
       {/* Values Section */}
       <div className="about-overview-values">
-        <h2>VALUES</h2>
+        <h2 className="about-heading">VALUES</h2>
         <ul className={`values-bullet-points ${showValues ? "hide" : ""}`}>
-          <li>Excellence & Integrity</li>
-          <li>Client-Focused</li>
-          <li>Sustainable Innovation</li>
+          <li className="about-list-item">Excellence & Integrity</li>
+          <li className="about-list-item">Client-Focused</li>
+          <li className="about-list-item">Sustainable Innovation</li>
         </ul>
-        <button onClick={toggleValues}>{showValues ? "-" : "+"}</button>
+        <button className="about-toggle-button" onClick={toggleValues}>
+          {showValues ? "-" : "+"}
+        </button>
         <div className={`values-details ${showValues ? "open" : ""}`}>
-          <p>
+          <p className="about-overview-details-text">
             {`Excellence & Integrity: We aim to provide high-quality service
             with dedication, trust, and confidentiality, Client-Focused Solutions:
             We focus on delivering fast, proactive, and mutually successful
--- ReactProject/cerasp/src/pages/About.js
+++ ReactProject/cerasp/src/pages/About.js
@@ -38,8 +38,8 @@ const About = () => {
   return (
     <div className="about scroll-container">
       {/* OVERVIEW */}
-      <ScrollPanel colorTheme="themeA" title="about" id="overview">
-        {/* <ThreeColumnWithHeaderPanel
+      {/* <ScrollPanel colorTheme="themeA" title="about" id="overview1">
+        <ThreeColumnWithHeaderPanel
           headerContent={
             <>
               <h1>History of Cerasp</h1>
@@ -59,7 +59,6 @@ const About = () => {
                 network, we are affiliated to John Abbott College and Cégep
                 Gérald-Godin and strives to deliver high-quality services
                 combining innovation and expertise.
-                
               </p>
             </>
           }
@@ -95,17 +94,13 @@ const About = () => {
                 service and the implementation of effective solutions to help
                 SMEs thrive. The CERASP prioritizes dedication and integrity,
                 focusing all its efforts on mutual success in an environment of
-                respect, loyalty, rigor, and openness. This will be achieved
-                through trust with our clients, proactivity, and fast service,
-                while ensuring confidentiality. Innovation in Sustainable
-                Development: Considering the international context of long term
-                climate change, the CERASP is committed to developing innovative
-                technologies that adhere to the principles of sustainable
-                development and environmental responsibility.
+                respect, loyalty, rigor, and openness.
               </p>
             </>
           }
-        /> */}
+        />
+      </ScrollPanel> */}
+      <ScrollPanel colorTheme="themeA" title="about" id="overview">
         <AboutOverview />
       </ScrollPanel>
 
@@ -196,16 +191,18 @@ const About = () => {
         <div className="videos-section">
           <h1>Videos</h1>
           <EmblaSinglePanel
-            slides={["blue", "red", "purple", "grey", "black"]}
+            slides={["ShwbF2xodT8", "ShwbF2xodT8", "ShwbF2xodT8"]}
             options={options}
             renderSlide={(slide) => (
-              <StockImageFetch
-                searchTerm={slide}
-                imgSource="large"
-                orientation="landscape"
-                page={1}
-                perPage={1}
-              />
+              <div className="video-container">
+                <iframe
+                  src={`https://www.youtube.com/embed/${slide}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
             )}
           />
         </div>
