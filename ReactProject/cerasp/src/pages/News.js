462d8f2530346fea6a6be09898591bf2da6aace3
nwpelletier <nickw.pelletier@gmail.com>
2025-04-09 18:25:24 -0400

    news test update

--- ReactProject/cerasp/src/pages/News.js
+++ ReactProject/cerasp/src/pages/News.js
@@ -6,51 +6,54 @@ import ContactUsForm from "../components/panels/ContactUsForm.js";
 import useScrollToPanel from "../hooks/useScrollToPanel.js";
 import ModalMessage from "../components/uicomponents/ModalMessage.js"; // Import the Modal
 import TestPanel from "../components/panels/TestPanel.js"; // Import the TestPanel component
+import { Scroll } from "lucide-react";
 
 const News = () => {
   return (
-    <div className="news-panel">
-      <div className="test-panel-1">
-        <h1>PANEL 1</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <ScrollPanel title="news" id="overview">
+      <div className="news-panel">
+        <div className="test-panel-1">
+          <h1>PANEL 1</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="test-panel-2">
+          <h1>PANEL 2</h1>
+          <p>
+            Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+            nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+            ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+          </p>
+        </div>
+        <div className="test-panel-3">
+          <h1>PANEL 3</h1>
+          <p>
+            Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
+            varius, turpis et commodo pharetra, est eros bibendum elit, nec
+            luctus magna felis sollicitudin mauris.
+          </p>
+        </div>
+        <div className="test-panel-1">
+          <h1>PANEL 1</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="test-panel-2">
+          <h1>PANEL 2</h1>
+          <p>
+            Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
+            nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
+            ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+          </p>
+        </div>
+        <div className="test-panel-3">
+          <h1>PANEL 3</h1>
+          <p>
+            Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
+            varius, turpis et commodo pharetra, est eros bibendum elit, nec
+            luctus magna felis sollicitudin mauris.
+          </p>
+        </div>
       </div>
-      <div className="test-panel-2">
-        <h1>PANEL 2</h1>
-        <p>
-          Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
-          nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-          Praesent mauris. Fusce nec tellus sed augue semper porta.
-        </p>
-      </div>
-      <div className="test-panel-3">
-        <h1>PANEL 3</h1>
-        <p>
-          Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
-          varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus
-          magna felis sollicitudin mauris.
-        </p>
-      </div>
-      <div className="test-panel-1">
-        <h1>PANEL 1</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      <div className="test-panel-2">
-        <h1>PANEL 2</h1>
-        <p>
-          Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
-          nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-          Praesent mauris. Fusce nec tellus sed augue semper porta.
-        </p>
-      </div>
-      <div className="test-panel-3">
-        <h1>PANEL 3</h1>
-        <p>
-          Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
-          varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus
-          magna felis sollicitudin mauris.
-        </p>
-      </div>
-    </div>
+    </ScrollPanel>
   );
 };
 
