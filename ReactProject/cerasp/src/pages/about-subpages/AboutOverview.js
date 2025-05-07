19dc6dd2459d8afdd2fc81d94770e6d90d3094a6
nwpelletier <nickw.pelletier@gmail.com>
2025-05-07 00:01:45 -0400

    update
    simplifying look/layout, as little css as possible but keeping it consistent

--- ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
+++ ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
@@ -21,8 +21,8 @@ export default function AboutOverview() {
           </p>
         </div>
 
-        <div className="about-overview-sections">
-          <div className="about-overview-mission">
+        <div className="about-overview-primary-sections">
+          <div className="about-overview-primary">
             <h2>MISSION</h2>
             <p>
               Advance research and training in pharmaceutical innovation to
@@ -32,7 +32,7 @@ export default function AboutOverview() {
               industry's growth and transformation.
             </p>
           </div>
-          <div className="about-overview-vision">
+          <div className="about-overview-primary">
             <h2>VISION</h2>
             <p>
               Be a leading partner in applied pharmaceutical research in Quebec
@@ -42,7 +42,7 @@ export default function AboutOverview() {
               worldwide to collaborate and advance pharmaceutical technologies.
             </p>
           </div>
-          <div className="about-overview-values">
+          <div className="about-overview-primary">
             <h2>VALUES</h2>
             <p>
               Excellence & Integrity: We aim to provide high-quality service
@@ -53,6 +53,56 @@ export default function AboutOverview() {
               responsibility.
             </p>
           </div>
+        </div>
+        <div class="about-overview-secondary-sections">
+          <details class="about-overview-secondary">
+            <summary>Excellence</summary>
+            <p>
+              The CERASP aims for excellence through quality service and the
+              implementation of effective solutions to help SMEs thrive. The
+              CERASP prioritizes dedication and integrity, focusing all its
+              efforts on mutual success in an environment of respect, loyalty,
+              rigor, and openness. This will be achieved through trust with our
+              clients, proactivity, and fast service, while ensuring
+              confidentiality.
+            </p>
+          </details>
+
+          <details class="about-overview-secondary">
+            <summary>Innovation in Sustainable Development</summary>
+            <p>
+              Considering the international context of long-term climate change,
+              the CERASP is committed to developing innovative technologies that
+              adhere to the principles of sustainable development and
+              environmental responsibility. The solutions offered to our clients
+              will be designed to reduce emissions and prioritize the prudent
+              use of raw materials, opting for processes with minimal
+              environmental impact. These solutions aim to change the behaviors
+              of users and consumers in the medium term.
+            </p>
+          </details>
+
+          <details class="about-overview-secondary">
+            <summary>Collaboration</summary>
+            <p>
+              The CERASP strives to attract and stimulate the creation of new
+              businesses by establishing an environment conducive to creativity,
+              applied research, and technological innovation in Quebec and
+              Canada. The CERASP aspires to be a central hub in its ecosystem,
+              positively contributing to the advancement of a modern and
+              innovative industry in the region. The partnership envisioned
+              between the two colleges serves as a perfect example of
+              solidarity.
+            </p>
+          </details>
+
+          <details class="about-overview-secondary">
+            <summary>Openness</summary>
+            <p>
+              Keeping an open mind to international developments and a
+              constantly evolving world.
+            </p>
+          </details>
         </div>
       </div>
     </div>
