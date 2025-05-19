95aeb10394bc4fa4f49f3c0b8c64eb5de798bdf2
nwpelletier <nickw.pelletier@gmail.com>
2025-05-19 16:28:20 -0400

    more updates
    going with dark theme for now

--- ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
+++ ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
@@ -1,4 +1,4 @@
-import "./css/AboutOverviewAlt.css";
+import "./css/AboutOverviewAltDark.css";
 import React, { useContext } from "react";
 import { LanguageContext } from "../../contexts/LanguageContext";
 import aboutTranslations from "./about-translations.json";
