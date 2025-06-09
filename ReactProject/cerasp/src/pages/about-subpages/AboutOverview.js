100eeed40b1263a36fa4a7b7fac30e808addd39c
nwpelletier <nickw.pelletier@gmail.com>
2025-06-09 16:20:29 -0400

    Update AboutOverview.js
    iframe fixes for JSX syntax (not really a bug but console warning comes up)

--- ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
+++ ReactProject/cerasp/src/pages/about-subpages/AboutOverview.js
@@ -37,10 +37,10 @@ export default function AboutOverview() {
               height="315"
               src="https://www.youtube.com/embed/6Wmb18o6Yos?si=5gXT4SvgAUZu-CEG"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           ) : (
             <iframe
@@ -48,10 +48,10 @@ export default function AboutOverview() {
               height="315"
               src="https://www.youtube.com/embed/ShwbF2xodT8?si=gexIqKRxD2kxLQVe"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           )}
 
