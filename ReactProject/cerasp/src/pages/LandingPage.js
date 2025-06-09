e8d6d91ed43ab4af44ece2b54293ede55e47120d
Sam Rivest <89708124+TwoShades@users.noreply.github.com>
2025-06-09 16:55:56 -0400

    -
    -

--- ReactProject/cerasp/src/pages/LandingPage.js
+++ ReactProject/cerasp/src/pages/LandingPage.js
@@ -4,20 +4,12 @@ import ContactUs from "./ContactUs/ContactUs.js";
 import Footer from "../components/layouts/Footer.js";
 import useScrollToPanel from "../hooks/useScrollToPanel.js";
 import UnderConstruction from "../components/layouts/UnderConstruction.js";
+import scrollToTop from "../utils/scrollToTop.js";
 
 import LandingHero from "./landing-subpages/LandingHero.js";
 
 const LandingPage = () => {
   useScrollToPanel();
-
-  return (
-    <div className="landing page-content">
-      <div id="landing-hero">
-        <LandingHero />
-
-import scrollToTop from "../utils/scrollToTop.js";
-
-const LandingPage = () => {
   scrollToTop();
   const [stage, setStage] = useState("logo");
 
@@ -33,72 +25,43 @@ const LandingPage = () => {
 
   return (
     <div className="landing page-content">
-      <div
-        className="background-blur"
-        style={{
-          backgroundImage: "url('/backgrounds/blueBacteriaBackground.jpg')",
-        }}
-      />
+      <div id="landing-hero">
+        <LandingHero />
 
-      <div className="content">
-        {/* ===================
+        <div className="content">
+          {/* ===================
             =====LANDING ZONE=====
             =================== */}
-        <div id="landing">
-          <div className="landing-bloc">
-            {stage === "logo" && (
-              <img
-                src="/logos/cerasplogo.png"
-                alt="CERASP Logo"
-                className="fade logo"
-              />
-            )}
-            {stage === "text" && (
-              <h1 className="fade text">Welcome to CERASP</h1>
-            )}
-            {stage === "page" && (
-              <div className="main-content-landing">
-                <UnderConstruction />
-              </div>
-            )}
+          <div id="landing">
+            <div className="landing-bloc">
+              {stage === "logo" && (
+                <img
+                  src="/logos/cerasplogo.png"
+                  alt="CERASP Logo"
+                  className="fade logo"
+                />
+              )}
+              {stage === "text" && (
+                <h1 className="fade text">Welcome to CERASP</h1>
+              )}
+              {stage === "page" && (
+                <div className="main-content-landing">
+                  <UnderConstruction />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div id="contact-us-form">
+            <ContactUs />
           </div>
         </div>
-
-        {/* ===================
-            =====OVERVIEW======
-            =================== */}
-        {/* <div id="overview">
-          <div className="landing-overview">
-            <h2>Overview</h2>
-            <p>
-              Learn more about our mission, our goals, and how CERASP is
-              revolutionizing education through immersive technologies.
-            </p>
-          </div>
-        </div> */}
-
-        {/* ============================
-            =====Equipment Updates======
-            ============================ */}
-        {/* <div id="welcome">
-          <div className="landing-welcome">
-            <h2>Equipment Updates</h2>
-            <p>
-              Stay up to date with the latest tools and features in our platform.
-            </p>
-          </div>
-        </div> */}
-
         <div id="contact-us-form">
           <ContactUs />
         </div>
 
+        <Footer />
       </div>
-      <div id="contact-us-form">
-        <ContactUs />
-      </div>
-
-      <Footer />
     </div>
   );
 };
