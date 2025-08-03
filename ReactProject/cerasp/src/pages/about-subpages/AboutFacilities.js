02df84ac8d510d17ee9a0c92d9bb8ec0c1db10ce
nwpelletier <nickw.pelletier@gmail.com>
2025-08-03 00:44:33 -0400

    facilities work
    blocked out new layout, needs work
    figures out "em", related to global font sizes... helps understand h1, h2, etc.
    
    todo: figure out map layout, make "locations" pop a bit more

--- ReactProject/cerasp/src/pages/about-subpages/AboutFacilities.js
+++ ReactProject/cerasp/src/pages/about-subpages/AboutFacilities.js
@@ -4,93 +4,93 @@ import { ScreenSizeContext } from "../../contexts/ScreenSizeContext";
 import { useContext } from "react";
 import React from "react";
 import SubPageHeader from "../../components/layouts/SubPageHeader";
+import RevealOnScroll from "../../components/uicomponents/RevealOnScroll";
 
 export default function AboutFacilities() {
   const { language } = useContext(LanguageContext);
   const { isMobile, isTablet, isFullScreen } = useContext(ScreenSizeContext);
   return (
     <div className="about-facilities">
-      <SubPageHeader
-        name={language === "fr" ? "LOCATIONS" : "FACILITIES"}
-        extraContent={
-          <h4>
+      <div className="about-facilities-intro">
+        <RevealOnScroll direction="right">
+          <h1>{language === "fr" ? "INSTALLATIONS" : "FACILITIES"}</h1>
+          <p>
             {language === "fr"
               ? "Le CERASP compte trois sites répartis à travers Montréal."
               : "The CERASP has three platforms available across Montreal."}
-          </h4>
-        }
-      />
-      <div className="about-facilities-layout">
-        <div className="about-facilities-images">
-          <div className="facility-card">
-            <img
-              src={`/photos/facilities/${
-                isFullScreen
-                  ? "john-abbott-college.jpg"
-                  : "john-abbott-college-landscape.jpg"
-              }`}
-              alt="John Abbott College"
-            />
-            <div className="facility-card-info">
-              {language === "fr" ? (
-                <h2>Cégep John Abbott</h2>
-              ) : (
-                <h2>John Abbott College</h2>
-              )}
-
-              <p>
-                {language === "fr"
-                  ? "Prototypage, fabrication à petit échelle et tests"
-                  : "Prototyping, small-scale manufacturing and testing"}
-              </p>
-            </div>
-          </div>
-
-          <div className="facility-card">
-            <img
-              src={`/photos/facilities/${
-                isFullScreen
-                  ? "cegep-gerald-godin.jpg"
-                  : "cegep-gerald-godin-landscape.jpg"
-              }`}
-              alt="Cégep Gérald-Godin"
-            />
-            <div className="facility-card-info">
-              <h2>Cégep Gérald-Godin</h2>
-              <p>
-                {language === "fr"
-                  ? "Prototypage, fabrication à petit échelle et tests"
-                  : "Prototyping, small-scale manufacturing and testing"}
-              </p>
-            </div>
-          </div>
-
-          <div className="facility-card">
-            <img
-              src={`/photos/facilities/${
-                isFullScreen ? "admare.jpg" : "admare-landscape.jpg"
-              }`}
-              alt="AdMare Facility"
-            />
-            <div className="facility-card-info">
-              <h2>AdMare BioInnovations</h2>
-              <p>
-                {language === "fr"
-                  ? "Laboratoire de recherche et développement"
-                  : "Research laboratory and development"}
-              </p>
-            </div>
-          </div>
+          </p>
+        </RevealOnScroll>
+      </div>
+      <div className="about-facilities-content">
+        <div className="facility-card facility-jac">
+          <img
+            src={`/photos/facilities/${
+              isFullScreen
+                ? "john-abbott-college.jpg"
+                : "john-abbott-college-landscape.jpg"
+            }`}
+            alt="John Abbott College"
+          />
         </div>
-
-        <div className="about-facilities-map">
-          <h2>{language === "fr" ? "Où nous trouver" : "Where to find us"}</h2>
+        <div className="facility-jac-info">
+          {" "}
           {language === "fr" ? (
-            <img src="/photos/facilities/cerasp-map-fr.jpg" alt="CERASP Map" />
+            <h2>Cégep John Abbott</h2>
           ) : (
-            <img src="/photos/facilities/cerasp-map-en.jpg" alt="CERASP Map" />
+            <h2>John Abbott College</h2>
           )}
+          <p>
+            {language === "fr"
+              ? "Prototypage, fabrication à petit échelle et tests"
+              : "Prototyping, small-scale manufacturing and testing"}
+          </p>
         </div>
+
+        <div className="facility-card facility-gg">
+          <img
+            src={`/photos/facilities/${
+              isFullScreen
+                ? "cegep-gerald-godin.jpg"
+                : "cegep-gerald-godin-landscape.jpg"
+            }`}
+            alt="Cégep Gérald-Godin"
+          />
+        </div>
+        <div className="facility-gg-info">
+          {" "}
+          <h2>Cégep Gérald-Godin</h2>
+          <p>
+            {language === "fr"
+              ? "Prototypage, fabrication à petit échelle et tests"
+              : "Prototyping, small-scale manufacturing and testing"}
+          </p>
+        </div>
+        <div className="facility-card facility-admare">
+          <img
+            src={`/photos/facilities/${
+              isFullScreen ? "admare.jpg" : "admare-landscape.jpg"
+            }`}
+            alt="AdMare Facility"
+          />
+        </div>
+        <div className="facility-admare-info">
+          {" "}
+          <h2>AdMare BioInnovations</h2>
+          <p>
+            {language === "fr"
+              ? "Laboratoire de recherche et développement"
+              : "Research laboratory and development"}
+          </p>
+        </div>
+      </div>
+
+      <div className="about-facilities-map">
+        <h2>{language === "fr" ? "Où nous trouver" : "Where to find us"}</h2>
+        {language === "fr" ? (
+          <img src="/photos/facilities/cerasp-map-fr.jpg" alt="CERASP Map" />
+        ) : (
+          <img src="/photos/facilities/cerasp-map-en.jpg" alt="CERASP Map" />
+        )}
       </div>
     </div>
   );
